c7dd36cdcb98381d069955bb7083bec629c60bc6
Anant Sethi <133277891+anantsethi11@users.noreply.github.com>
2024-07-11 12:44:24 +0530

    Update Linear.py

--- Linear.py
+++ Linear.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 data = pd.read_csv("1.01. Simple linear regression.csv")
 data
-data.describe()
 y= data['GPA']
 x1 = data['SAT']
 plt.scatter(x1,y)
@@ -13,7 +12,6 @@ plt.ylabel('GPA', fontsize = 20)
 plt.show()
 x = sm.add_constant(x1)
 result = sm.OLS(y,x).fit()       #method that applies a specific estimation technique to obtain the fit of the model.
-result.summary()
 plt.scatter(x1,y)
 plt.xlabel('SAT', fontsize = 20)
 plt.ylabel('GPA', fontsize = 20)
